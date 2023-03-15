b39f7333b0f45cf6fb58fb8f35ef73d0cc44920f
Sumit-Dhakal <gorkha202@gmail.com>
2023-03-15 18:51:19 +0545

    changes

--- src/component/pages/NotFound.js
+++ src/component/pages/NotFound.js
@@ -4,6 +4,9 @@ const NotFound = () => {
   return (
     <div>
       <h1 className='text-center mt-10 text-2xl text-rose-400'>Page Not Found</h1>
+      <div className='h-[450px]'>
+        <lottie-player src="https://assets9.lottiefiles.com/packages/lf20_gyNnzxbm7F.json" background="transparent" speed="1" loop autoplay></lottie-player>
+      </div>
     </div>
   )
 }
